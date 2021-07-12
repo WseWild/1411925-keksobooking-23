28c14aac0faf6e05aeb34b0e7f624c3d423b2b8d
WseWild <wsevs@protonmail.com>
2021-07-12 21:06:36 +0300

    Фикс магических чисел

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,8 +1,20 @@
 const MIN_NAME_LENGTH = 30;
 const MAX_NAME_LENGTH = 100;
+const numberOfRooms = {
+  ONE_ROOM: '1',
+  TWO_ROOMS: '2',
+  THREE_ROOMS: '3',
+  ONE_HUNDRED_ROOMS: '100',
+};
+const numberOfGuests = {
+  ONE_GUEST: '1',
+  TWO_GUESTS: '2',
+  THREE_GUESTS: '3',
+  NOT_FOR_GUESTS: '0',
+};
+
 
 const formTitleInput = document.querySelector('.ad-form__title');
-
 const formCapacity = document.querySelector('#capacity');
 const formRooms = document.querySelector('#room_number');
 const adForm = document.querySelector('.ad-form');
@@ -24,13 +36,13 @@ adForm.addEventListener('input', (evt) => {
   if (evt.target.matches('#room_number') || evt.target.matches('#capacity') ) {
     formCapacity.setCustomValidity('');
 
-    if (formRooms.value === '1' && formCapacity.value !== '1') {
+    if (formRooms.value === numberOfRooms.ONE_ROOM && formCapacity.value !== numberOfGuests.ONE_GUEST ) {
       formCapacity.setCustomValidity('1 комната доступна только «для 1 гостя»');
-    } else if (formRooms.value === '2' && formCapacity.value !== '1' && formCapacity.value !== '2') {
+    } else if (formRooms.value === numberOfRooms.TWO_ROOMS && formCapacity.value !== numberOfGuests.ONE_GUEST && formCapacity.value !== numberOfGuests.TWO_GUESTS) {
       formCapacity.setCustomValidity('2 комнаты доступны «для 2 гостей» или «для 1 гостя»');
-    } else if (formRooms.value === '3' && formCapacity.value === '0' ) {
+    } else if (formRooms.value === numberOfRooms.THREE_ROOMS  && formCapacity.value === numberOfGuests.NOT_FOR_GUESTS) {
       formCapacity.setCustomValidity('3 комнаты доступны «для 3 гостей» «для 2 гостей» и «для 1 гостя»');
-    } else if (formRooms.value === '100' && formCapacity.value !== '0') {
+    } else if (formRooms.value === numberOfRooms.ONE_HUNDRED_ROOMS && formCapacity.value !== numberOfGuests.NOT_FOR_GUESTS) {
       formCapacity.setCustomValidity('100 комнат доступно только «не для гостей».');
     }
 
